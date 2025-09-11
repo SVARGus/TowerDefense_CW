6d15c7a92597b1db05e537b1d5188cdeae6a69c8
SVARGus <kyznetsovpn@mail.ru>
2025-09-11 22:18:39 +0300

    Рабочие правки, в http все отробатывает, но в https запуске выдает ошибку

--- TowerDefense_CW/wwwroot/js/TD/Canvas.js
+++ TowerDefense_CW/wwwroot/js/TD/Canvas.js
@@ -5,8 +5,9 @@ export const Canvas = {
     _graphicEngine: new GraphicEngine(),
 
     
-    __temp(_this) {
-        _this._graphicEngine.Stop();
+    __temp() {
+        this._graphicEngine.Stop();
+        console.log("GraphicEngine остановлен __temp");
     },
 
     /**
@@ -17,23 +18,34 @@ export const Canvas = {
     Init(id) {
         this._graphicEngine.Init();
         this._graphicEngine.Start();
-        setTimeout(this.__temp, 5000);
+        setTimeout(this.__temp.bind(this), 5000);
 
         /**
          * @type {HTMLCanvasElement}
          */
         let gameField = document.getElementById(id);
+        if (!gameField || !(gameField instanceof HTMLCanvasElement)) {
+            console.error("Canvas not found or is not canvas:", id, gameField);
+            return;
+        }
         let canvas = document.getElementById(id);
 
         console.log("gameField получено [", gameField, "].",
             " Проверка на тип - [", gameField instanceof HTMLCanvasElement, "]");
 
         let context = gameField.getContext('2d');
+        if (!context) {
+            console.error("2D context not available");
+            return;
+        }
 
+        context.save();
+        context.beginPath();
         context.setTransform(1, 0, 0, 1, 100, 100);
         context.rotate((Math.PI / 180) * 45);
         context.rect(-25, -25, 50, 50);
         context.stroke();
+        context.restore();
     },
 
 }
